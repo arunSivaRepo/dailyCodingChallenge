a87e23f989a7da5012b342ecfad2c348b85f0fde
practiceproblems <g.arun@yahoo.com>
2014-06-25 01:29:59 -0700

    Deadlock avoidance

--- c++/multithreading/deadlock_avoidance.cpp
+++ c++/multithreading/deadlock_avoidance.cpp
@@ -9,7 +9,7 @@ std::mutex mu1,mu2;
 void print1(const std::string &s)
 {
   std::lock(mu1, mu2);
-  std::lock_guard<std::mutex> l(mu1, std::adopt_lock);
+  std::lock_guard<std::mutex> l(mu1, std::adopt_lock); // get the ownership of mu1.
   std::chrono::microseconds ns(10); // force deadlock
   std::this_thread::sleep_for(ns);
   std::lock_guard<std::mutex> l2(mu2, std::adopt_lock);
@@ -21,7 +21,8 @@ void print1(const std::string &s)
 
 void print2(const std::string &s)
 {
-  std::unique_lock<std::mutex> l(mu1, std::defer_lock);
+  std::unique_lock<std::mutex> l(mu1, std::defer_lock); // lock_gaurd will lock immediately. unique_lock can defer, 
+                                                        // lock and unlock how many ever times.
   std::chrono::microseconds ns(10);
   std::this_thread::sleep_for(ns);
   l.lock();
